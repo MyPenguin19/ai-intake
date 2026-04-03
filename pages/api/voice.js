d65ed103ef4699e9fee91feb540d67a9d4b3542b
MyPenguin19 <97816660+MyPenguin19@users.noreply.github.com>
2026-04-03 00:20:19 -0400

    Add project files

---
+++ pages/api/voice.js
@@ -0,0 +1,30 @@
+import twilio from "twilio";
+
+function buildGather(response, prompt) {
+  const gather = response.gather({
+    input: "speech",
+    action: "/api/process",
+    method: "POST",
+    speechTimeout: "auto",
+  });
+
+  gather.say(prompt);
+}
+
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", "POST");
+    return res.status(405).send("Method Not Allowed");
+  }
+
+  const twiml = new twilio.twiml.VoiceResponse();
+
+  buildGather(
+    twiml,
+    "Thanks for calling Inside Diagnostics. What seems to be the issue?",
+  );
+  twiml.redirect({ method: "POST" }, "/api/voice");
+
+  res.setHeader("Content-Type", "text/xml");
+  res.status(200).send(twiml.toString());
+}
